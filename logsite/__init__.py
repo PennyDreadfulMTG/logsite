0d7558573298f45ec69e59ced0374fb128370fff
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-01-12 00:05:42 +1100

    Activate API

--- logsite/__init__.py
+++ logsite/__init__.py
@@ -3,6 +3,6 @@ import subprocess
 from flask import Flask
 APP = Flask(__name__)
 
-from . import main
+from . import main, api
 
 APP.config['commit-id'] = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
