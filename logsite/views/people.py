8f5bf00ae615bd8f600e4effa8fcd6ed4a1323e3
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-02-13 01:26:54 +1100

    Favorite Format

--- logsite/views/people.py
+++ logsite/views/people.py
@@ -1,4 +1,5 @@
 from flask import url_for
+from sqlalchemy import func
 
 from logsite.view import View
 from .. import db
@@ -22,3 +23,5 @@ class People(View):
     def prepare(self):
         for p in self.people:
             p.num_matches = match.get_recent_matches_by_player(p.name).count()
+            p.formats = db.db.session.query(match.Match, func.count(match.Match.format_id)).filter(match.Match.players.any(db.User.id == p.id)).group_by(match.Match.format_id).order_by(func.count(match.Match.format_id).desc()).all()
+            p.fav_format = '{0} ({1} matches)'.format(p.formats[0][0].format.get_name(), p.formats[0][1])
