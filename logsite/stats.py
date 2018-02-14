d162b8ddd96a1ad5a6b021dfd8ea26d66f7ca3a6
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-02-14 15:58:37 +1100

    More granularity, more useful data.

--- logsite/stats.py
+++ logsite/stats.py
@@ -18,28 +18,40 @@ def stats():
     last_week = dtutil.now() - dtutil.ts2dt(7 * 24 * 60 * 60)
     for m in base_query.group_by(match.Match.format_id).filter(match.Match.start_time > last_week).order_by(func.count(match.Match.format_id).desc()).all():
         f = m[0].format
-        val['formats'][f.name]['last_week'] = m[1]
+        val['formats'][f.name]['last_week'] = {}
+        val['formats'][f.name]['last_week']['num_matches'] = m[1]
+        stmt = text("""select b.* from user as b
+                        inner join (
+                            select user.id from user
+                            left join match_players
+                            ON match_players.user_id = user.id
+                            left join `match`
+                            on `match`.id = match_players.match_id
+                            where `match`.format_id = :fid and `match`.start_time is not null and
+                            `match`.start_time > date_sub(now(), interval 7 DAY)
+                            group by user.id
+                        ) as a on a.id = b.id
+                        """)
+        players = db.db.session.query(db.User).from_statement(stmt).params(fid=f.id).all()
+        val['formats'][f.name]['last_week']['recent_players'] = [p.name for p in players]
     last_month = dtutil.now() - dtutil.ts2dt(30 * 24 * 60 * 60)
     for m in base_query.group_by(match.Match.format_id).filter(match.Match.start_time > last_month).order_by(func.count(match.Match.format_id).desc()).all():
         f = m[0].format
-        val['formats'][f.name]['last_month'] = m[1]
-    for f in db.Format.query.all():
-        if f.name in val['formats'].keys():
-            stmt = text("""select b.* from user as b
-                                inner join (
-                                    select user.id from user
-                                    left join match_players
-                                    ON match_players.user_id = user.id
-                                    left join `match`
-                                    on `match`.id = match_players.match_id
-                                    where `match`.format_id = :fid and `match`.start_time is not null and
-                                    `match`.start_time > date_sub(now(), interval 30 DAY)
-                                    group by user.id
-                                ) as a on a.id = b.id
-                                """)
-            players = db.db.session.query(db.User).from_statement(stmt).params(fid=f.id).all()
-            val['formats'][f.name]['recent_players'] = [p.name for p in players]
-
-
+        val['formats'][f.name]['last_month'] = {}
+        val['formats'][f.name]['last_month']['num_matches'] = m[1]
+        stmt = text("""select b.* from user as b
+                        inner join (
+                            select user.id from user
+                            left join match_players
+                            ON match_players.user_id = user.id
+                            left join `match`
+                            on `match`.id = match_players.match_id
+                            where `match`.format_id = :fid and `match`.start_time is not null and
+                            `match`.start_time > date_sub(now(), interval 30 DAY)
+                            group by user.id
+                        ) as a on a.id = b.id
+                        """)
+        players = db.db.session.query(db.User).from_statement(stmt).params(fid=f.id).all()
+        val['formats'][f.name]['last_month']['recent_players'] = [p.name for p in players]
     return return_json(val)
 
